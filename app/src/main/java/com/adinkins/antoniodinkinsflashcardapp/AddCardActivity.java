4722a70196e708709f87b7b497f8aeaee3ade3d0
Antonio Dinkins <afdinki@gmail.com>
2020-03-07 01:35:09 -0500

    Added Lab2 Requirements

---
+++ app/src/main/java/com/adinkins/antoniodinkinsflashcardapp/AddCardActivity.java
@@ -0,0 +1,64 @@
+package com.adinkins.antoniodinkinsflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        /* When the Cancel Button is clicked, it should return to the Main Screen */
+        findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // finish() method closes the current activity screen
+                finish();
+            }
+        });
+
+        //Add hint to txtNewQuestion
+        TextView defaultQuestionText = findViewById(R.id.txtNewQuestion);
+        defaultQuestionText.setHint(getResources().getString(R.string.new_question_hint));
+
+        //Add hint to txtNewAnswer
+        TextView defaultAnswerText = findViewById(R.id.txtNewAnswer);
+        defaultAnswerText.setHint(getResources().getString(R.string.new_answer_hint));
+
+        /* When the save button is clicked, it should do the following:
+            - Take the inputs from txtNewQuestion and txtNewAnswer and pass them to the MainActivity
+            - Close the current Activity
+         */
+        findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText question = findViewById(R.id.txtNewQuestion); // Get question field
+                String questionString = question.getText().toString(); // Sets input text to String variable
+
+                EditText answer = findViewById(R.id.txtNewAnswer);  // Get answer field
+                String answerString = answer.getText().toString();  // Sets input text to String variable
+
+                // Takes data and sends it to the MainActivity
+                // fcData = flashcard data
+                Intent fcData = new Intent();                           // Creates a new Intent
+                fcData.putExtra("Question", questionString);     // Key-value pair for the question string
+                fcData.putExtra("Answer", answerString);         // Key-value pair for the answer string
+                setResult(RESULT_OK, fcData);                           // sends Result code plus data to original activity
+                finish();                                               // Closes current activity
+
+
+
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/adinkins/antoniodinkinsflashcardapp/MainActivity.java
+++ app/src/main/java/com/adinkins/antoniodinkinsflashcardapp/MainActivity.java
@@ -2,18 +2,36 @@ package com.adinkins.antoniodinkinsflashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean isShowingAnswers = true;
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 100 && (data != null)){
+            String question = data.getExtras().getString("Question"); // Gets question data from AddCardActivity and sets it to a string variable
+            String answer = data.getExtras().getString("Answer");    // Gets answer data from the AddCardActivity and sets it to a string variable
+
+            //Take data and display it in text fields
+            TextView questionField = findViewById(R.id.txtQuestion);
+            TextView answerField = findViewById(R.id.txtAnswer);
+
+            questionField.setText(question);
+            answerField.setText(answer);
+        }
+
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         findViewById(R.id.flashcard_app).setBackgroundColor(getResources().getColor(R.color.app_bgcolor, null));
-        findViewById(R.id.txtQuestion).setBackgroundColor(getResources().getColor(R.color.question_bgcolor, null));
+        //findViewById(R.id.txtQuestion).setBackgroundColor(getResources().getColor(R.color.question_bgcolor, null));
 
 
 
@@ -34,7 +52,94 @@ public class MainActivity extends AppCompatActivity {
             findViewById(R.id.txtQuestion).setVisibility(View.VISIBLE);
         }
     });
+
+    findViewById(R.id.txtChoice1).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView firstChoice = findViewById(R.id.txtChoice1);
+            String firstChoiceText = firstChoice.getText().toString();
+            if(firstChoiceText.equals(getResources().getString(R.string.answer))){
+                findViewById(R.id.txtChoice1).setBackgroundColor(getResources().getColor(R.color.answer_bgcolor, null));
+            }
+            else {
+                findViewById(R.id.txtChoice1).setBackgroundColor(getResources().getColor(R.color.wrong_answer, null));
+            }
+        }
+    });
+
+
+    findViewById(R.id.txtChoice2).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            TextView secondChoice = findViewById(R.id.txtChoice2);
+            String secondChoiceText = secondChoice.getText().toString();
+            if(secondChoiceText.equals(getResources().getString(R.string.answer))){
+                findViewById(R.id.txtChoice2).setBackgroundColor(getResources().getColor(R.color.answer_bgcolor, null));
+            }
+            else {
+                findViewById(R.id.txtChoice2).setBackgroundColor(getResources().getColor(R.color.wrong_answer, null));
+            }
+        }
+    });
+
+    findViewById(R.id.txtChoice3).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView thirdChoice = findViewById(R.id.txtChoice3);
+            String thirdChoiceText = thirdChoice.getText().toString();
+            if(thirdChoiceText.equals(getResources().getString(R.string.answer))){
+                findViewById(R.id.txtChoice3).setBackgroundColor(getResources().getColor(R.color.answer_bgcolor, null));
+            }
+            else {
+                findViewById(R.id.txtChoice3).setBackgroundColor(getResources().getColor(R.color.wrong_answer, null));
+            }
+        }
+    });
+
+
+        findViewById(R.id.toggleChoicesVisibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShowingAnswers) {
+                    ((ImageView) findViewById(R.id.toggleChoicesVisibility)).setImageResource(R.drawable.show_answers);
+                    findViewById(R.id.txtChoice1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.txtChoice2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.txtChoice3).setVisibility(View.INVISIBLE);
+                    isShowingAnswers = false;
+
+                }
+                else {
+                    ((ImageView)findViewById(R.id.toggleChoicesVisibility)).setImageResource(R.drawable.hide_answers);
+                    findViewById(R.id.txtChoice1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.txtChoice2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.txtChoice3).setVisibility(View.VISIBLE);
+                    isShowingAnswers = true;
+                }
+            }
+        });
+
+
+        /* Add onClickListener to Add Button
+        onClicklistener should forward user to the AddCardActivity activity screen
+         */
+        findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    /* The following code creates a connection between MainActivity and AddCardActivity
+                        The way we connect Activities is by defining an Intent and passing that intent
+                        from one Activity to another
+
+                     */
+                    Intent addCardIntent = new Intent(MainActivity.this, AddCardActivity.class);
+                    MainActivity.this.startActivityForResult(addCardIntent, 100);
+            }
+        });
+
+
+
     }
+
 
 
 }
